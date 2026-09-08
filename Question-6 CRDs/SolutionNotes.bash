699c9024376b3dab3f539bc2c181be886dca28e5
sherivnk <32613061+sherivnk@users.noreply.github.com>
2026-09-08 12:52:26 -0400

    command updated for actual crds yaml format

--- Question-6 CRDs/SolutionNotes.bash
+++ Question-6 CRDs/SolutionNotes.bash
@@ -1,5 +1,5 @@
 # List cert-manager CRDs and save
-kubectl get crd | grep cert-manager | tee /root/resources.yaml
+kubectl get crd -l app.kubernetes.io/instance=cert-manager -o yaml > /root/resources.yaml
 
 # Save spec subject explain output
-kubectl explain certificate.spec.subject | tee /root/subject.yaml
+kubectl explain certificate.spec.subject > /root/subject.yaml
