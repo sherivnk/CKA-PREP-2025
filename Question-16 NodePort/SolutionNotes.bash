a608eb89a038ac7fd0bfa94cfb81e29ff9e9bf92
sherivnk <32613061+sherivnk@users.noreply.github.com>
2026-09-23 21:10:05 -0400

    Update deployment and add NodePort service instructions
    Updated the deployment configuration and added instructions to create a NodePort service. Included commands for editing the deployment and generating the service YAML file.

--- Question-16 NodePort/SolutionNotes.bash
+++ Question-16 NodePort/SolutionNotes.bash
@@ -1,9 +1,31 @@
 # Add container port to deployment
-kubectl patch deployment nodeport-deployment -n relative -p '{
-  "spec":{"template":{"spec":{"containers":[{
-    "name":"nginx",
-    "ports":[{"name":"http","containerPort":80,"protocol":"TCP"}]
-  }]}}}}'
+
+kubectl edit deployment nodeport-deployment
+
+spec:
+  template:
+    spec:
+      containers:
+        - name: <container-name> # Keep the existing container name
+          ports:                 # Add this four lines from here
+            - name: http
+              containerPort: 80
+              protocol: TCP
+
+create yaml file from the command line or K8s document
+
+kubectl expose deployment nodeport-deployment \
+  --name=nodeport-service \
+  --type=NodePort \
+  --port=80 \
+  --target-port=80 \
+  --protocol=TCP \
+  --dry-run=client -o yaml > nodeport-service.yaml
+  
+vim nodeport-service.yaml 
+nodePort: 30080 #add  this two lines after dryrun 
+name: http
+
 kubectl get deploy nodeport-deployment -n relative -o wide
 
 # Create NodePort service on 30080
